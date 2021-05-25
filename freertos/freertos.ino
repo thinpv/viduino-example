023fc68c392df857f60539d9c3a370fed48c3d67
thinpv <thindcna@gmail.com>
2021-05-25 16:19:45 +0700

    add freertos

---
+++ freertos/freertos.ino
@@ -0,0 +1,56 @@
+
+void printTask(void *parameter);
+void blinkTask(void *parameter);
+// the setup function runs once when you press reset or power the board
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println("setup");
+
+  pinMode(LED_BUILTIN, OUTPUT);
+
+  xTaskCreate(
+      printTask,    /* Task function. */
+      "print Task", /* name of task. */
+      10000,        /* Stack size of task */
+      NULL,         /* parameter of the task */
+      1,            /* priority of the task */
+      NULL);        /* Task handle to keep track of created task */
+
+  xTaskCreate(
+      blinkTask,    /* Task function. */
+      "blink Task", /* name of task. */
+      10000,        /* Stack size of task */
+      NULL,         /* parameter of the task */
+      1,            /* priority of the task */
+      NULL);        /* Task handle to keep track of created task */
+  vTaskDelete(NULL);
+}
+
+void printTask(void *parameter)
+{
+  for (;;)
+  {
+    Serial.println("this is print Task");
+    delay(1000);
+  }
+  vTaskDelete(NULL);
+}
+
+void blinkTask(void *parameter)
+{
+  for (;;)
+  {
+    digitalWrite(LED_BUILTIN, HIGH); // turn the LED on (HIGH is the voltage level)
+    delay(100);                      // wait for a second
+    digitalWrite(LED_BUILTIN, LOW);  // turn the LED off by making the voltage LOW
+    delay(100);
+  }
+  vTaskDelete(NULL);
+}
+
+// the loop function runs over and over again forever
+void loop()
+{
+  delay(100); // wait for a second
+}
