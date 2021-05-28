3a87dadfd1e8813d44b4de5954b69e9f2c595bc0
thinpv <thindcna@gmail.com>
2021-05-28 13:55:16 +0700

    add lvgl 7.11.0 add example

---
+++ lv_demo_printer/lv_demo_printer.h
@@ -0,0 +1,41 @@
+/**
+ * @file lv_demo_printer.h
+ *
+ */
+
+#ifndef LV_DEMO_PRINTER_H
+#define LV_DEMO_PRINTER_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/*********************
+ *      INCLUDES
+ *********************/
+// #include "../../lv_examples.h"
+#include <lvgl.h>
+
+/*********************
+ *      DEFINES
+ *********************/
+#define LV_USE_DEMO_PRINTER 1
+
+/**********************
+ *      TYPEDEFS
+ **********************/
+
+/**********************
+ * GLOBAL PROTOTYPES
+ **********************/
+void lv_demo_printer(void);
+
+/**********************
+ *      MACROS
+ **********************/
+
+#ifdef __cplusplus
+} /* extern "C" */
+#endif
+
+#endif /*LV_DEMO_PRINTER_H*/
